6d1dbb93873cc3fc822592270f0d268c9b2d15a9
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-06-27 03:51:12 +0000

    Completed user fragment
    move on to implement tabs

--- app/src/main/java/dev/gitly/view/user/UserFragment.kt
+++ app/src/main/java/dev/gitly/view/user/UserFragment.kt
@@ -7,14 +7,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 import dev.gitly.R
 import dev.gitly.databinding.FragmentUserBinding
 import dev.gitly.debugger
+import dev.gitly.view.user.tabs.MentorBlogPostsFragment
+import dev.gitly.view.user.tabs.MentorExperienceFragment
+import dev.gitly.view.user.tabs.MentorReviewFragment
+import dev.gitly.view.user.tabs.PersonalInfoFragment
 import dev.gitly.viewmodel.UserViewModel
+
 
 @AndroidEntryPoint
 class UserFragment : Fragment() {
@@ -22,6 +31,7 @@ class UserFragment : Fragment() {
 
     private val userViewModel by viewModels<UserViewModel>()
     private val args by navArgs<UserFragmentArgs>()
+    private val pageTitles = mutableListOf("Personal Info", "Experiences", "Reviews", "Blog posts")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -60,6 +70,77 @@ class UserFragment : Fragment() {
             }
         }
 
+        binding.run {
+            userPager.adapter = ViewPageFragmentAdapter(requireActivity())
+            TabLayoutMediator(tabs, userPager) { tab, position ->
+                tab.text = pageTitles[position]
+            }.attach()
+            userPager.setPageTransformer(DepthPageTransformer())
+            executePendingBindings()
+        }
+
+    }
+
+    /**
+     * Adapter for [ViewPager2]
+     */
+    internal class ViewPageFragmentAdapter constructor(context: FragmentActivity) :
+        FragmentStateAdapter(context) {
+
+        override fun getItemCount(): Int = 4
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> PersonalInfoFragment()
+                1 -> MentorExperienceFragment()
+                2 -> MentorReviewFragment()
+                else -> MentorBlogPostsFragment()
+            }
+        }
+
+    }
+
+    internal class DepthPageTransformer : ViewPager2.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            val pageWidth = view.width
+            when {
+                position < -1 -> { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    view.alpha = 0f
+                }
+                position <= 0 -> { // [-1,0]
+                    // Use the default slide transition when moving to the left page
+                    view.alpha = 1f
+                    view.translationX = 0f
+                    view.translationZ = 0f
+                    view.scaleX = 1f
+                    view.scaleY = 1f
+                }
+                position <= 1 -> { // (0,1]
+                    // Fade the page out.
+                    view.alpha = 1 - position
+
+                    // Counteract the default slide transition
+                    view.translationX = pageWidth * -position
+                    // Move it behind the left page
+                    view.translationZ = -1f
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    val scaleFactor = (MIN_SCALE
+                            + (1 - MIN_SCALE) * (1 - Math.abs(position)))
+                    view.scaleX = scaleFactor
+                    view.scaleY = scaleFactor
+                }
+                else -> { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    view.alpha = 0f
+                }
+            }
+        }
+
+        companion object {
+            private const val MIN_SCALE = 0.75f
+        }
     }
 
 
