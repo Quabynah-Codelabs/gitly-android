b5bcda96eee9f229121960d6a83b20902e636acd
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-06-28 01:14:51 +0000

    Update WebService.kt

--- mobile/app/src/main/java/dev/gitly/model/sources/remote/service/WebService.kt
+++ mobile/app/src/main/java/dev/gitly/model/sources/remote/service/WebService.kt
@@ -10,40 +10,40 @@ import retrofit2.http.*
 interface WebService {
 
     @Headers("Accept: application/json")
-    @POST("/gitly-mobile/us-central1/gitly/oauth")
-//    @POST("/gitly/oauth")
+//    @POST("/gitly-mobile/us-central1/gitly/oauth")
+    @POST("/gitly/oauth")
     suspend fun login(
         @Body loginRequest: LoginRequest
     ): AccessToken
 
     @Headers("Accept: application/json")
-    @POST("/gitly-mobile/us-central1/gitly/users")
-//    @POST("/gitly/users")
+//    @POST("/gitly-mobile/us-central1/gitly/users")
+    @POST("/gitly/users")
     suspend fun signUp(
         @Body registerRequest: RegisterRequest
     ): User
 
     @Headers("Accept: application/json")
-    @GET("/gitly-mobile/us-central1/gitly/users/me")
-//    @GET("/gitly/users/me")
+//    @GET("/gitly-mobile/us-central1/gitly/users/me")
+    @GET("/gitly/users/me")
     suspend fun getCurrentUser(): User?
 
     @Headers("Accept: application/json")
-    @GET("/gitly-mobile/us-central1/gitly/users/{id}")
-//    @GET("/gitly/users/{id}")
+//    @GET("/gitly-mobile/us-central1/gitly/users/{id}")
+    @GET("/gitly/users/{id}")
     suspend fun getUserById(@Path("id") id: String): User?
 
     @Headers("Accept: application/json")
-    @PUT("/gitly-mobile/us-central1/gitly/users/me")
-//    @PUT("/gitly/users/me")
+//    @PUT("/gitly-mobile/us-central1/gitly/users/me")
+    @PUT("/gitly/users/me")
     suspend fun updateUserAsync(@Body user: User): User
 
-    @DELETE("/gitly-mobile/us-central1/gitly/users/{id}")
-//    @DELETE("/gitly/users/{id}")
+//    @DELETE("/gitly-mobile/us-central1/gitly/users/{id}")
+    @DELETE("/gitly/users/{id}")
     suspend fun deleteUser(@Path("id") userId: String?)
 
-    @POST("/gitly-mobile/us-central1/gitly/users/mentors")
-//    @POST("/gitly/users/mentors")
+//    @POST("/gitly-mobile/us-central1/gitly/users/mentors")
+    @POST("/gitly/users/mentors")
     suspend fun requestMentors(
         @Body request: PagingRequest
     ): List<User>
