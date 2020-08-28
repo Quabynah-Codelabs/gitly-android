a8854d4f56aa13f9b2a871e8203b71a3b765aec1
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-08-28 17:08:59 +0000

    Added compose template application

---
+++ compose/app/src/main/java/dev/gitly/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.gitly
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
+import androidx.ui.material.MaterialTheme
+import androidx.ui.material.Surface
+import androidx.ui.tooling.preview.Preview
+import dev.gitly.ui.GitlyTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitlyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GitlyTheme {
+        Greeting("Android")
+    }
+}
